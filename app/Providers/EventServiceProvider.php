a21d2c113e404c7dc33083f35acb1b6f30e070b6
Ignas Navickas <ignas.navickas@lirema.lt>
2023-03-08 07:44:29 +0200

    total fix

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\Report;
 use App\Models\User;
+use App\Observers\ReportObserver;
 use App\Observers\UserObserver;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
@@ -29,6 +31,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Report::observe(ReportObserver::class);
         User::observe(UserObserver::class);
     }
 }
